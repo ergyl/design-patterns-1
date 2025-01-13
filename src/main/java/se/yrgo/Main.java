2f89690a701118767c328e6c2621d5f9fd77a9b0
Erik Gylleus <144366622+ergyl@users.noreply.github.com>
2025-01-13 13:29:45 +0100

    add given code for task 1

--- src/main/java/se/yrgo/Main.java
+++ src/main/java/se/yrgo/Main.java
@@ -1,2 +1,17 @@
-package se.yrgo;public class Main {
+package se.yrgo;
+
+import se.yrgo.task1.Person;
+import se.yrgo.task1.Storage;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Person> persons = Storage.fetchPersons();
+
+        // Make the capacity no bigger than the number of elements
+        persons.trimToSize();
+
+        System.out.println(persons);
+    }
 }
--- src/main/java/se/yrgo/task1/Person.java
+++ src/main/java/se/yrgo/task1/Person.java
@@ -1,2 +1,22 @@
-package se.yrgo.task1;public class Person {
+package se.yrgo.task1;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public class Person implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 3487495895819393L;
+    private String name;
+    private String email;
+
+    public Person(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s - %s", name, email);
+    }
 }
