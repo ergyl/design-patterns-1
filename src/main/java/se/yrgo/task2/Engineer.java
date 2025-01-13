70ff9f41e6214650545a06909b87cd3ec23859ea
Erik Gylleus <144366622+ergyl@users.noreply.github.com>
2025-01-13 15:04:19 +0100

    add given code for task 2

---
+++ src/main/java/se/yrgo/task2/Engineer.java
@@ -0,0 +1,12 @@
+package se.yrgo.task2;
+
+public class Engineer extends Employee {
+    public Engineer(String name, String department) {
+        super(name, department);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Engineer %s", super.toString());
+    }
+}
---
+++ src/main/java/se/yrgo/task2/Manager.java
@@ -0,0 +1,12 @@
+package se.yrgo.task2;
+
+public class Manager extends Employee {
+    public Manager(String name, String department) {
+        super(name, department);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Manager %s", super.toString());
+    }
+}
