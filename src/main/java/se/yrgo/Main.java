3ebf91e11420811472f503df9d6bb927d28833d3
Erik Gylleus <144366622+ergyl@users.noreply.github.com>
2025-01-13 15:17:33 +0100

    make Employee concrete class and add Role class

--- src/main/java/se/yrgo/Main.java
+++ src/main/java/se/yrgo/Main.java
@@ -3,6 +3,8 @@ package se.yrgo;
 import se.yrgo.task1.Person;
 import se.yrgo.task1.PersonPopulator;
 import se.yrgo.task1.Storage;
+import se.yrgo.task2.Employee;
+import se.yrgo.task2.Role;
 
 import java.util.List;
 
@@ -16,5 +18,14 @@ public class Main {
         List<Person> persons = Storage.fetchPersons();
 
         System.out.println(persons);
+
+        Employee employee1 = new Employee("John Doe", "HR",
+                new Role("Administrator"));
+
+        System.out.println(employee1);
+        System.out.println();
+        System.out.println("Adding a new role...");
+        employee1.addRole(new Role("Team coordinator"));
+        System.out.println(employee1);
     }
 }
--- src/main/java/se/yrgo/task2/Employee.java
+++ src/main/java/se/yrgo/task2/Employee.java
@@ -1,12 +1,21 @@
 package se.yrgo.task2;
 
-public abstract class Employee {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Employee {
     private String name;
     private String department;
+    private List<Role> roles;
 
-    public Employee(String name, String department) {
-        this.name = name;
-        this.department = department;
+    public Employee(String name, String department, Role initalRole) {
+        this.name = Objects.requireNonNull(name, "Name must be chosen for employee.");
+        this.department = Objects.requireNonNull(department, "Department must be chosen for employee");
+        this.roles = new ArrayList<>();
+        if (initalRole != null) {
+            this.roles.add(initalRole);
+        }
     }
 
     public String name() {
@@ -17,8 +26,18 @@ public abstract class Employee {
         return department;
     }
 
+    public List<Role> roles() {
+        return roles;
+    }
+
+    public void addRole(Role role) {
+        if (!roles.contains(role) && role != null) {
+            this.roles.add(role);
+        }
+    }
+
     @Override
     public String toString() {
-        return String.format("%s at %s", name, department);
+        return String.format("%s at %s with role/s: %s", name, department, roles);
     }
 }
