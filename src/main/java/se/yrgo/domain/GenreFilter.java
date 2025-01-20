f11bd5dbddea4487fe79fb5016f77d77312e4a56
Erik Gylleus <144366622+ergyl@users.noreply.github.com>
2025-01-20 17:05:31 +0100

    add interface GenreFilter

--- src/main/java/se/yrgo/domain/GenreFilter.java
+++ src/main/java/se/yrgo/domain/GenreFilter.java
@@ -1,4 +1,13 @@
 package se.yrgo.domain;
 
 public interface GenreFilter {
+
+    /**
+     * A method that should filter out records
+     * based on chosen preferences for filter.
+     *
+     * @return true if the record should be filtered out
+     * (=not included) in the list of albums.
+     */
+    public abstract boolean filterOut();
 }
